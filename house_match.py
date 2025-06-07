cab84530d81316a6a7154ddf1843476f871b2c3d
Mikołaj 140635 <pzx112240@student.poznan.merito.pl>
2025-06-07 11:45:19 +0200

    match functionality (switch,case)

---
+++ house_match.py
@@ -0,0 +1,11 @@
+﻿name = input("Whats your name?").capitalize()
+
+match name:
+    case "Harry" | "Hermione":
+        print("Gryffindor")
+    #case "Hermione":
+        #print("Gryffindor")
+    case "Draco":
+        print("Slytherin")
+    case _:
+        print("Who?")
