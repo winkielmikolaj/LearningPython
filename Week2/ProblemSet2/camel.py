d0528bf6ea62c9412707d75df7000ac774138ed2
Mikołaj 140635 <pzx112240@student.poznan.merito.pl>
2025-06-09 13:31:15 +0200

    camelCase to snake_case

---
+++ Week2/ProblemSet2/camel.py
@@ -0,0 +1,10 @@
+﻿camelCase = input("camelCase: ")
+snake_case = ""
+
+for c in camelCase:
+    if c.isupper():
+        snake_case += "_" + c.lower()
+    else:
+        snake_case += c
+
+print(snake_case)
