56b924e12b531d6b25f92eaebafaa28e958ed739
Mikołaj Winkiel <winkielmikolaj03@gmail.com>
2025-06-23 10:20:24 +0200

    Exceptions -> try, except with while

---
+++ number.py
@@ -0,0 +1,15 @@
+﻿def main():
+    x = get_int("What is x")
+    print(f"x is {x}")
+
+def get_int(prompt):
+    while True:
+        try:
+            x = int(input(prompt))
+        except ValueError:
+            print("X is not an integer!!! Try again")
+        else:
+            return x
+
+
+main()
