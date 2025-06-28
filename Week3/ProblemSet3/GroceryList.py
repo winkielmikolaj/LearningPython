e6dbab19b89cec5b2c538262d985a84e0f0e567f
Mikołaj Winkiel <winkielmikolaj03@gmail.com>
2025-06-28 12:33:57 +0200

    Problem3 in Week3

---
+++ Week3/ProblemSet3/GroceryList.py
@@ -0,0 +1,13 @@
+﻿
+groceryList = {}
+x = 0
+try:
+	while True:
+		x += 1
+		item = input().upper()
+		groceryList[item] = x
+		continue
+	print(groceryList)
+except EOFError:
+	for item, count in groceryList.items():
+		print(f"{count}. {item}")
