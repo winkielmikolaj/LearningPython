901623bf599bf6ff1b0d1b7891a4f31dd4036d42
Mikołaj Winkiel <winkielmikolaj03@gmail.com>
2025-06-30 10:29:58 +0200

    library random

---
+++ random.py
@@ -0,0 +1,11 @@
+﻿import random
+
+#coin = random.choice(["heads", "tails"])
+#number = random.randint(1,10)
+
+#print(number)
+
+cards = ["jack", "queen", "king"]
+random.shuffle(cards)
+for card in cards:
+	print(card)
