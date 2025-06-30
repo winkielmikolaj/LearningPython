df0cc3e9702b5b601ac9b61bda5939b1a0579d95
Mikołaj Winkiel <winkielmikolaj03@gmail.com>
2025-06-30 10:40:04 +0200

    library sys

---
+++ sys.py
@@ -0,0 +1,7 @@
+﻿import sys
+
+#you have to start this program by terminal to work properly
+try:
+	print("Hello, my name is", sys.argv[1])
+except IndexError:
+	print("Too few arguments. Write your name or try to run this program by terminal")
