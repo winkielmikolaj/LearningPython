b062cf51166397b44dd86bb0d6f4965afcd4a61b
Mikołaj Winkiel <winkielmikolaj03@gmail.com>
2025-07-01 09:19:46 +0200

    API of Art in chicago

---
+++ api.py
@@ -0,0 +1,23 @@
+﻿import requests
+
+def main():
+	print("Search the Museum")
+	artist = input("Artist: ")
+
+	n = 0
+	try:
+		response = requests.get(
+			"https://api.artic.edu/api/v1/artworks/search",
+			{"q": artist}
+		)
+		response.raise_for_status()
+	except requests.HTTPError:
+		print("Couldn't complete request")
+		return
+
+	content = response.json()
+	for artwork in content["data"]:
+		n += 1
+		print(f"{n} {artwork['title']}")
+
+main()
